05ca8e7a30fc84634ef5bcf1d02a3efcffbc29e8
Niraj Jha <niraj.jha@primefocus.com>
2018-09-20 17:06:59 +0530

    Initial Commit

---
+++ DeviceDiagnostic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeviceDiagnostic
+//
+//  Created by Niraj Jha on 20/09/18.
+//  Copyright © 2018 Niraj Jha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
